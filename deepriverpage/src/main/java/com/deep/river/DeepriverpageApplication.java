fd16a2ebe981e32209e424a58bc5e6fedb1937a2
yhyuh1000 <85008564+yhyuh1000@users.noreply.github.com>
2021-05-30 19:49:13 +0900

    add spring project

---
+++ deepriverpage/src/main/java/com/deep/river/DeepriverpageApplication.java
@@ -0,0 +1,13 @@
+package com.deep.river;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepriverpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepriverpageApplication.class, args);
+	}
+
+}
---
+++ deepriverpage/src/test/java/com/deep/river/DeepriverpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deep.river;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepriverpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
